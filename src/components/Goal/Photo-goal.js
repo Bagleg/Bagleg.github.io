d5f46c3f11e3d2ac4c88fea9968359e7d27ef4ca
Bagleg <joshuacoreymartinez@gmail.com>
2023-04-17 08:49:20 -0400

    Updates

--- src/components/Goal/Photo-goal.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Photo() {
-  return (
-    <div>
-        <img src={require('../images/NY.jpg')}></img>
-    </div>
-  )
-}
--- src/components/Homepage/Photo-home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Photo() {
-  return (
-    <div>
-        <img className='center' src={require('../images/margherita-pizza-sliced-food-italian.jpg')}></img>
-    </div>
-  )
-}
