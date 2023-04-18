088dc538732120aa17fd79ffba69a2b63640ced8
Joshua Martinez <86437288+Bagleg@users.noreply.github.com>
2023-04-18 13:38:05 -0400

    adding react links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,45 +1,46 @@
 import React from 'react'
 import './Navbar.css'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 
 export default function Navbar() {
   return (
     <div>
         <ul className='list'>
-           <a href='/'>
+            <Link to="/">
                 <li>
                     <h4>
                         Home
                     </h4>
-            </li>
-            </a>
-            <a href='/goal'>
+                </li>
+            </Link>
+            <Link to="/goal">
                 <li>
                     <h4>
                         My goal
                     </h4>
                 </li>
-            </a>
-            <a href='/past'>
+            </Link>
+            <Link to="/past">
                 <li>
                     <h4>
                         Past Research
                     </h4>
                 </li>
-            </a>
-            <a href='/research'>
+            </Link>
+            <Link to="/research">
                 <li>
                     <h4>
                         My Research
                     </h4>
                 </li>
-            </a>
-            <a href='/conclusion'>
+            </Link>
+            <Link to="/conclusion">
                 <li>
                     <h4>
                         Conclusion
                     </h4>
                 </li>
-            </a>
+            </Link>
         </ul>
     </div>
   )
